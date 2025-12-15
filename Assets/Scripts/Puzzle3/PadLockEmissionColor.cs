31b6e57dc5d9c219c3e08eb598b7fc30777d60ce
Abdullah Bawazeer <145490935+abawzr@users.noreply.github.com>
2025-12-16 02:35:11 +0300

    fixed puzzle 3 blinking

--- Assets/Scripts/Puzzle3/PadLockEmissionColor.cs
+++ Assets/Scripts/Puzzle3/PadLockEmissionColor.cs
@@ -6,34 +6,56 @@ public class PadLockEmissionColor : MonoBehaviour
 {
     TimeBlinking tb;
 
-    private GameObject _myRuller;
+    private Renderer _renderer;
+    private Material _materialInstance;
+    private static readonly int EmissionColorProperty = Shader.PropertyToID("_EmissionColor");
 
     [HideInInspector]
     public bool _isSelect;
 
-    //[SerializeField] private float _timeBlinking = 0.5f;
-
     private void Awake()
     {
         tb = FindObjectOfType<TimeBlinking>();
+        _renderer = GetComponent<Renderer>();
+
+        if (_renderer != null)
+        {
+            _materialInstance = new Material(_renderer.sharedMaterial);
+            _renderer.material = _materialInstance;
+            _materialInstance.EnableKeyword("_EMISSION");
+        }
     }
-    void Start()
+
+    private void Update()
     {
-        _myRuller = gameObject;
+        BlinkingMaterial();
+    }
+
+    private void OnDestroy()
+    {
+        if (_materialInstance != null)
+        {
+            Destroy(_materialInstance);
+        }
     }
 
     public void BlinkingMaterial()
     {
-        _myRuller.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        if (_materialInstance == null) return;
+
+        _materialInstance.EnableKeyword("_EMISSION");
 
         if (_isSelect)
         {
-            _myRuller.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.Lerp(Color.clear, Color.darkGreen, Mathf.PingPong(Time.time, tb.blinkingTime)));
-        }
-        if (_isSelect == false)
-        {
-            _myRuller.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.clear);
-        }
+            float t = Mathf.PingPong(Time.time, tb.blinkingTime) / tb.blinkingTime;
+            Color emissionColor = Color.Lerp(Color.black, Color.green * 0.2f, t);
 
+            _materialInstance.SetColor(EmissionColorProperty, emissionColor);
+        }
+        else
+        {
+            _materialInstance.SetColor(EmissionColorProperty, Color.black);
+        }
     }
 }
+
